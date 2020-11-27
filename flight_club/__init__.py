7c551f0f627081d9847d4a774c22fa093ffcf7bf
dcuomo56 <dcuomo56@gmail.com>
2020-11-27 17:31:29 -0500

    basic 404 error handling for invalid users or sessions, as well a custom 404 error page

--- flight_club/__init__.py
+++ flight_club/__init__.py
@@ -12,11 +12,14 @@ TEST_CSV = 'fc_test.csv'
 
 db = SQLAlchemy()
 
+def page_not_found(e):
+  return render_template('404.html'), 404
 
 def create_app(test_config=None):
     """Create and configure the app
     """
     app = Flask(__name__, instance_relative_config=True)
+    app.register_error_handler(404, page_not_found)
 
     # (TODO) figure out how to like use flask config system
     import flight_club.fc_config as fc_config
--- flight_club/users/views.py
+++ flight_club/users/views.py
@@ -1,33 +1,38 @@
 import functools
 
-from flask import Blueprint, flash, g, redirect, render_template, request, session, url_for
+from flask import Blueprint, flash, g, redirect, render_template, request, session, url_for, abort
 
 from flight_club import db
 from flight_club.models.models import User, Beer, Session
 from flight_club.auth.views import login_required
 from flight_club.users.fc_member import FCMember
+import flight_club.models.db_helper as db_helper
 
 bp = Blueprint('users', __name__, url_prefix='/users')
 
 # TODO (dan) move this to a profile view
 @bp.route('/<user_id>', methods=['GET'])
 def user_page(user_id):
-
+    # If this isn't a valid user, send a 404.
+    if not db_helper.check_if_user_exists(user_id):
+        abort(404)
     user = FCMember(user_id)
-    return render_template('users/profile.html', 
-                            user=user.username,
-                            score=user.avg_score, 
-                            beers=user.beers,
-                            win_total=user._win_count,
-                            wins=user.wins)
+    return render_template('users/profile.html',
+                           user=user.username,
+                           score=user.avg_score,
+                           beers=user.beers,
+                           win_total=user._win_count,
+                           wins=user.wins)
+
 
 @bp.route('/profile', methods=['GET'])
 @login_required
 def profile():
     return user_page(g.username)
 
+
 @bp.route('/list', methods=['GET'])
 @login_required
 def list_sessions():
     users = User.query.all()
-    return render_template('users/users_list.html',users=users)  
+    return render_template('users/users_list.html', users=users)
