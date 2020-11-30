00c9757d0cacf6587643f35d91940c70ed88e85e
dcuomo56 <dcuomo56@gmail.com>
2020-11-29 23:27:06 -0500

    remove specifying port

--- app.py
+++ app.py
@@ -3,5 +3,5 @@ import os
 
 if __name__ == "__main__":
     app = create_app()
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host = '0.0.0.0', port=port)
+    #port = int(os.environ.get('PORT', 5000))
+    app.run(host = '0.0.0.0')
