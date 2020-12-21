855f7e8ed647056117617995cff2c8997f57cce3
Dan <dcuomo56@gmail.com>
2020-12-21 10:44:20 -0500

    added validators for input forms

--- flight_club/sessions/views.py
+++ flight_club/sessions/views.py
@@ -19,64 +19,215 @@ from flight_club import db
 from flight_club.models.models import Beer, Session
 from flight_club.auth.views import login_required
 from flight_club.sessions.fc_sessions import FCSession
+from werkzeug.exceptions import BadRequestKeyError
 
+import datetime
 import flight_club.models.db_func as db_func
 
 bp = Blueprint("sessions", __name__, url_prefix="/sessions")
+
+
+def session_input_validator(session_id, date, host):
+    """This function is for validation the inputs for the session portion
+    of add session. It will type check and value check.
+
+    Args:
+        session_id : the id of the session
+        date : the date of the session
+        host : the host of the
+
+    Returns:
+        boolean indicating if checks failed
+        error string, either error or None
+        session object or None if errors
+
+    """
+    # Session id checks
+    try:
+        session_id = int(session_id)
+    except:
+        return False, "Session_id needs to be a positive integer", None
+
+    if not db_func.check_if_session_exists(session_id):
+        return False, "Session already exists", None
+
+    if session_id <= 0:
+        return False, "Session id should be a positive number, greater than 0", None
+
+    # Date checks
+    # Note date is passed as YYYY-MM-DD we want to store and display MM/DD/YYYY.
+    # Mostly because we are dumb Americans.
+    try:
+        date_object = datetime.datetime.strptime(date, "%Y-%m-%d")
+    except ValueError:
+        # This should never happen, since we use a date input selector.
+        # Maybe on day someone will attempt to hack us via the date field?
+        return False, "Date was not inputted in YYYY-MM-DD format", None
+
+    date = datetime.date.strftime(date_object, "%m/%d/%Y")
+
+    # Host checks
+    if not db_func.check_if_user_exists(host):
+        return False, "Username {} does not exist".format(host), None
+
+    return True, None, Session(id=session_id, date=date)
+
+
+def beer_input_validator(
+    beer_name, beer_abv, brewery, style, votes, win, username, session_id
+):
+    """This function is for validating the input from the beer add session form.
+    It checks that a few database constraints are met prior to adding,
+    as well as values are in the correct format.
+
+    Args:
+        beer_name : The string for the beer name.
+        beer_abv : A string that will be converted to abv.
+        brewery : A string that is the name of the brewery.
+        style : A string that is the style of the beer.
+        votes : The number of votes this beer got.
+        win : An integer that represents if the beer won the session.
+        username : The user who brought the beer
+        session_id : The session the beer is in
+
+    Returns:
+        boolean indicating if checks failed
+        error string, either error or None
+        beer object or None if errors
+    """
+
+    # Checks for beer name.
+    if beer_name == "":
+        return False, "Beer name should not be an empty string", None
+
+    # Checks for brewery
+    if brewery == "":
+        return (
+            False,
+            "Brewery for {} should not be an empty string".format(beer_name),
+            None,
+        )
+
+    # Checks for abv
+    try:
+        beer_abv = float(beer_abv)
+    except:
+        return (
+            False,
+            "Beer Abv for {} needs to be a valid decimal number".format(beer_abv),
+            None,
+        )
+
+    if beer_abv < 0:
+        return False, "Beer Abv for {} should not be negative".format(beer_abv), None
+
+    # Checks for style
+    if style == "":
+        return False, "Style for {} should not be an empty string".format(style), None
+
+    # Checks for vote
+    try:
+        votes = int(votes)
+    except:
+        return (
+            False,
+            "Votes for {} should be a valid positive integer".format(votes),
+            None,
+        )
+
+    if votes < 0:
+        return False, "Votes for {} should not be a negative number".format(votes), None
+
+    # Checks for win
+    try:
+        win = int(win)
+    except:
+        return False, "Win should be either 1 or 0".format(win), None
+
+    if win < 0 or win > 1:
+        return False, "Win should be either 1 or 0".format(win), None
+
+    # Check for username
+    if not db_func.check_if_user_exists(username):
+        return False, "Username {} does not exist".format(username), None
+
+    # Not validating session here, as it's validated in a different part of the form.
+    # Maybe will change this later
+
+    return (
+        True,
+        None,
+        Beer(
+            beer_name=beer_name,
+            beer_abv=beer_abv,
+            brewery=brewery,
+            style=style,
+            votes=votes,
+            win=win,
+            username=username,
+            session_id=session_id,
+        ),
+    )
 
 
 @bp.route("/add_session", methods=["GET", "POST"])
 @login_required
 def add_session():
     if request.method == "POST":
-        print(request.form)
         if not request.form["session_id"]:
             error = "There needs to be a session id"
         if not request.form["date"]:
             error = "There needs to be a date"
-        if not request.form["beers"] or int(request.form["beers"]) == 0:
-            error = "There needs to be at least one beer, but one beer would be a lonely flight club dog dog dog dog dog dog dog "
+        # TODO (dan) I need to actually tie the hosts in.
+        if not request.form["host"]:
+            error = "There needs to be a host"
         else:
             # Check Session isn't duplicate
-            session_id = request.form["session_id"]
-            if db_func.check_if_session_exists(session_id):
-                error = "This is a duplicate session"
+            session_valid, error, new_session = session_input_validator(
+                request.form["session_id"], request.form["date"], request.form["host"]
+            )
+            if not session_valid:
+                flash(error)
+                return render_template("sessions/add_session.html")
             else:
-                # Add the session
-                date = request.form["date"]
-                db.session.add(Session(id=session_id, date=date))
-                db.session.commit()
+                # Construct and add the session
+                db.session.add(new_session)
 
-                # Extract Number of Beers
-                num_beers = int(request.form["beers"])
+                # Loop Through Form to get Beers
+                still_beers = True
+                i = 0
+                while still_beers:
+                    try:
+                        beer_name = request.form["beer_{}".format(i)]
+                        beer_abv = float(request.form["beer_abv_{}".format(i)])
+                        brewery = request.form["brewery_{}".format(i)]
+                        style = request.form["style_{}".format(i)]
+                        votes = request.form["votes_{}".format(i)]
+                        win = request.form["win_{}".format(i)]
+                        username = request.form["username_{}".format(i)]
 
-                # Loop Through Form
-                # TODO (dan) there is a way for this form to submit without there being beers
-                # should probably fix the front end..
-                for i in range(num_beers):
-                    beer_name = request.form["beer_name{}".format(i)]
-                    beer_abv = float(request.form["beer_abv{}".format(i)])
-                    brewery = request.form["brewery{}".format(i)]
-                    style = request.form["style{}".format(i)]
-                    votes = request.form["votes{}".format(i)]
-                    win = request.form["win{}".format(i)]
-                    username = request.form["username{}".format(i)]
-
-                    db.session.add(
-                        Beer(
-                            beer_name=beer_name,
-                            beer_abv=beer_abv,
-                            brewery=brewery,
-                            style=style,
-                            votes=votes,
-                            win=win,
-                            username=username,
-                            session_id=session_id,
+                        beer_valid, error, new_beer = beer_input_validator(
+                            beer_name,
+                            beer_abv,
+                            brewery,
+                            style,
+                            votes,
+                            win,
+                            username,
+                            session_id,
                         )
-                    )
+                        if beer_valid:
+                            db.session.add(new_beer)
+                            i += 1
+                        else:
+                            flash(error)
+                            return render_template("sessions/add_session.html")
+                    except BadRequestKeyError:
+                        still_beers = False
 
-                    db.session.commit()
-                return redirect(url_for("sessions.view_session",id=session_id))
+                # Commit at the end, dont' want to commit half things.
+                db.session.commit()
+                return redirect(url_for("sessions.view_session", id=session_id))
 
         flash(error)
         return render_template("sessions/add_session.html")
